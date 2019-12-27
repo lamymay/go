470b6e76a7227e28b75ea69b7da6c31e3ee2f4cf
lamymay <lamymay@outlook.com>
2019-12-27 17:55:51 +0800

    add test

---
+++ src/0-test/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	defer_call()
+}
+
+//defer的执行顺序是倒序执行（同入栈先进后出） 栈结构，FILO
+//1.defer的生效顺序
+
+//2.defer与return,函数返回值之间的顺序		return最先执行->return负责将结果写入返回值中->接着defer开始执行一些收尾工作->最后函数携带当前返回值退出
+//
+//返回值的表达方式，我们知道根据是否提前声明有两种方式：一种是func test() int 另一种是 func test() (i int)，所以两种情况都来说说
+
+
+//3.defer定义和执行两个步骤，做的事情。
+func defer_call() {
+	defer func() { fmt.Println("打印前") }()
+	defer func() { fmt.Println("打印中") }()
+	defer func() { fmt.Println("打印后") }()
+
+	panic("触发异常")
+}
---
+++ src/2-number/main.go
@@ -0,0 +1,8 @@
+package main
+
+	import "fmt"
+
+func main() {
+	val1 := 1
+	fmt.Print("类型是=%T",val1)
+}
